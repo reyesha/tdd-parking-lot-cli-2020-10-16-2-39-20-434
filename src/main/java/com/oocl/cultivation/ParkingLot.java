e2d68b7d85fed3969afa3f8cfd6a837bca621023
Hannah Mae Reyes <reyesha@oocl.com>
2020-10-18 02:31:33 +0800

    [ParkingLot] [Hannah] feature: execute return no car park failed and no ticket returned given parking capacity is 1

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,11 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
+    private final int parkingLotCapacity = 1;
     private final Map<ParkingTicket,Car> ticketCarMap = new HashMap<>();
     public ParkingTicket park(Car car) {
         ParkingTicket ticket = new ParkingTicket();
-        ticketCarMap.put(ticket,car);
-        return ticket;
+
+        if (ticketCarMap.size() == parkingLotCapacity){
+            return null;
+        }else {
+            ticketCarMap.put(ticket,car);
+            return ticket;
+        }
     }
 
     public Map<ParkingTicket, Car> getTicketCarMap() {
