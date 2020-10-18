d3c4e9c660af51d39f5dad6cfa93bbfbf7c3e06f
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-18 22:23:57 +0800

    [ParkingLot] [Hannah] add: added UnrecognizedParkingTicketException class

---
+++ src/main/java/com/oocl/cultivation/UnrecognizedParkingTicketException.java
@@ -0,0 +1,7 @@
+package com.oocl.cultivation;
+
+public class UnrecognizedParkingTicketException extends Exception {
+    public UnrecognizedParkingTicketException(String message) {
+        super(message);
+    }
+}
