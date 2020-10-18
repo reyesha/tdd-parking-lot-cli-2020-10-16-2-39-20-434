806b9b30be29c208e28f956412f75a91518084df
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-18 22:23:37 +0800

    [ParkingLot] [Hannah] add: added ProvideParkingTicketException class

---
+++ src/main/java/com/oocl/cultivation/ProvideParkingTicketException.java
@@ -0,0 +1,7 @@
+package com.oocl.cultivation;
+
+public class ProvideParkingTicketException extends Exception {
+    public ProvideParkingTicketException(String message) {
+        super(message);
+    }
+}
