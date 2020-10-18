d91ee7fed92effbc13e1b04317f82eb6e1e5e2e6
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-18 22:40:58 +0800

    [ParkingLot] [Hannah] add: added NotEnoughPositionException class

---
+++ src/main/java/com/oocl/cultivation/NotEnoughPositionException.java
@@ -0,0 +1,7 @@
+package com.oocl.cultivation;
+
+public class NotEnoughPositionException extends Exception {
+    public NotEnoughPositionException(String message) {
+        super(message);
+    }
+}
