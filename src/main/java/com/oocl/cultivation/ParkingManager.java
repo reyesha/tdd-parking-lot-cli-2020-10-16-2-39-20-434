db6a756c96528abc289ec8f9cbba5c9dce890d1a
Hannah Mae Reyes <reyesha@oocl.com>
2020-10-20 00:25:18 +0800

    [ParkingLot] [Hannah] feature: execute command for manager can park/fetch car in his/her parking lot

--- src/main/java/com/oocl/cultivation/ParkingManager.java
+++ src/main/java/com/oocl/cultivation/ParkingManager.java
@@ -3,8 +3,12 @@ package com.oocl.cultivation;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ParkingManager {
+public class ParkingManager extends ParkingBoy {
     public List<ParkingBoy> managedParkingBoys = new ArrayList<>();
+
+    public ParkingManager(List<ParkingLot> parkingLotList, ParkingStrategy parkingStrategy) {
+        super(parkingLotList, parkingStrategy);
+    }
 
     public void manage(ParkingBoy parkingBoy) {
         this.managedParkingBoys.add(parkingBoy);
