c47ac48d93c8edf88975791b1c727e94f3477813
Hannah Mae Reyes <reyesha@oocl.com>
2020-10-20 00:24:56 +0800

    [ParkingLot] [Hannah] test: added test for manager can park/fetch car in his/her parking lot

--- src/test/java/com/oocl/cultivation/ParkingManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingManagerTest.java
@@ -12,9 +12,13 @@ class ParkingManagerTest {
         //given
         List<ParkingLot> parkingLotList1 = new ArrayList<>();
         List<ParkingLot> parkingLotList2 = new ArrayList<>();
+        List<ParkingLot> parkingLotList = new ArrayList<>();
+        parkingLotList.add(new ParkingLot(10));
+        parkingLotList1.add(new ParkingLot(10));
+        parkingLotList2.add(new ParkingLot(10));
 
         //when
-        ParkingManager parkingManager = new ParkingManager();
+        ParkingManager parkingManager = new ParkingManager(parkingLotList, new NormalParkingStrategy());
         ParkingBoy parkingBoy1 = new ParkingBoy(parkingLotList1, new NormalParkingStrategy());
         ParkingBoy parkingBoy2 = new ParkingBoy(parkingLotList2, new SmartParkingStrategy());
         parkingManager.manage(parkingBoy1);
@@ -22,5 +26,24 @@ class ParkingManagerTest {
 
         //then
         assertEquals(2, parkingManager.managedParkingBoys.size());
+    }
+
+    @Test
+    void should_return_manager_can_park_or_fetch_when_adding_parking_boy_to_management_list_given_2_parking_boys() throws Exception {
+        //given
+        List<ParkingLot> parkingLotList1 = new ArrayList<>();
+        List<ParkingLot> parkingLotList2 = new ArrayList<>();
+        parkingLotList1.add(new ParkingLot(10));
+        parkingLotList2.add(new ParkingLot(10));
+        Car car = new Car();
+
+        //when
+        ParkingManager parkingManager = new ParkingManager(parkingLotList1, new NormalParkingStrategy());
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotList2, new NormalParkingStrategy());
+        parkingManager.manage(parkingBoy);
+        parkingManager.park(car);
+
+        //then
+        assertEquals(1, parkingLotList1.size());
     }
 }
