d517d4690f295b852849d31259c29947cc00cbbb
Hannah Mae Reyes <reyesha@oocl.com>
2020-10-19 23:12:43 +0800

    [ParkingLot] [Hannah] add: added SuperSmartParkingStrategy class

---
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingStrategy.java
@@ -0,0 +1,8 @@
+package com.oocl.cultivation;
+
+public class SuperSmartParkingStrategy implements ParkingStrategy {
+    @Override
+    public ParkingTicket park(ParkingBoy parkingBoy, Car car) throws Exception {
+        return null;
+    }
+}
