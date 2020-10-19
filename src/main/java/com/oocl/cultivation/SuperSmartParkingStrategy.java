95d81499feac62337266a2732d7d3ed1e0bab0a8
Hannah Mae Reyes <reyesha@oocl.com>
2020-10-19 23:31:10 +0800

    [ParkingLot] [Hannah] feature: added logic for SuperSmartParkingBoy

--- src/main/java/com/oocl/cultivation/SuperSmartParkingStrategy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingStrategy.java
@@ -3,6 +3,18 @@ package com.oocl.cultivation;
 public class SuperSmartParkingStrategy implements ParkingStrategy {
     @Override
     public ParkingTicket park(ParkingBoy parkingBoy, Car car) throws Exception {
-        return null;
+        ParkingLot preferredParkingLot = parkingBoy.parkingLotList.get(0);
+        for(ParkingLot lot : parkingBoy.parkingLotList) {
+            int availableParkingSlot1 =  lot.getParkingLotCapacity() - lot.getParkedCars();
+            double positionRateParkingLot1 = availableParkingSlot1/lot.getParkingLotCapacity();
+            int availableParkingSlot2 = preferredParkingLot.getParkingLotCapacity() - preferredParkingLot.getParkedCars();
+            int positionRateParkingLot2 = availableParkingSlot2/preferredParkingLot.getParkingLotCapacity();
+
+            if(positionRateParkingLot1 > positionRateParkingLot2){
+                preferredParkingLot = lot;
+            }
+        }
+
+        return preferredParkingLot.park(car);
     }
 }
